3026860e52e120e23aae40af77aa1346bed0162f
dglovesa <estudianteutulpz@gmail.com>
2026-08-01 21:03:19 -0300

    Added secret message interactions

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ function cambiarModo() {
         cuerpo.style.color = '#f4f1ea';
     }
 }
+
+function revelarSecret() {
+    const elemento = document.getElementById('mensaje-sorpresa');
+    elemento.classList.remove('oculto');
+
+    // Opcional: Cerrar el mensaje si vuelve a hacer clic en cualquier lado
+    setTimeout(() => {
+        window.addEventListener('click', function cerrar(e) {
+            if (!elemento.contains(e.target) && !e.target.classList.contains('secreto')) {
+                elemento.classList.add('oculto');
+                window.removeEventListener('click', cerrar);
+            }
+        });
+    }, 100);
+}
