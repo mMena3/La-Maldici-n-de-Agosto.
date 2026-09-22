9fe92d77b5e2382923837e8096bbaa7fc54dba31
dglovesa <estudianteutulpz@gmail.com>
2026-09-22 20:43:21 -0300

    I changed my mind

--- poem-script.js
+++ poem-script.js
@@ -1,20 +1,9 @@
-// Agrega aquí las estrofas de tu soneto
+// Agrega aquí las estrofas de tu soneto (usa <br> para los saltos de línea)
 const estrofas = [
   "Un verso aquí que empieza la penumbra,<br>otro verso que sigue en la distancia,<br>la luz lejana que apenas alumbra,<br>y el susurro tenue de su fragancia.",
   "Segunda estrofa del soneto aquí,<br>continúa el ritmo con elegancia,<br>palabras dulces que guardé para ti,<br>llenas de calma y de resonancia.",
   "Tercera estrofa que empieza a cerrar,<br>con sentimiento firme y profundo,<br>un verso claro para recordar.",
-  "Y en el último verso de este mundo,<br>dejar la huella que no ha de borrar,<br>este secreto sutil y fecundo.",
-  
-  // Corazón alineado con proporciones reales:
-  "<div class='corazon-puntos'>" +
-  " * *       * * <br>" +
-  "*   *     *   *<br>" +
-  "*     * *     *<br>" +
-  " *           * <br>" +
-  "   *       *   <br>" +
-  "     *   *     <br>" +
-  "       *       " +
-  "</div>"
+  "Y en el último verso de este mundo,<br>dejar la huella que no ha de borrar,<br>este secreto sutil y fecundo."
 ];
 
 const contenedor = document.getElementById("contenedor-poema");
@@ -22,7 +11,7 @@ let indiceEstrofa = 0;
 
 function escribirEstrofa() {
   if (indiceEstrofa < estrofas.length) {
-    const p = document.createElement("div");
+    const p = document.createElement("p");
     p.classList.add("estrofa-tipeando");
     contenedor.appendChild(p);
 
@@ -30,27 +19,21 @@ function escribirEstrofa() {
     let i = 0;
 
     function escribirLetra() {
-      // Reconoce saltos de línea y el contenedor del corazón
+      // Si encontramos una etiqueta <br>, la insertamos completa de golpe
       if (textoHTML.substring(i, i + 4) === "<br>") {
         p.innerHTML += "<br>";
         i += 4;
-      } else if (textoHTML.substring(i, i + 28) === "<div class='corazon-puntos'>") {
-        p.innerHTML += "<div class='corazon-puntos'>";
-        i += 28;
-      } else if (textoHTML.substring(i, i + 6) === "</div>") {
-        p.innerHTML += "</div>";
-        i += 6;
       } else {
         p.innerHTML += textoHTML.charAt(i);
         i++;
       }
 
       if (i < textoHTML.length) {
-        setTimeout(escribirLetra, 20); // Velocidad de tipeo fluida
+        setTimeout(escribirLetra, 40); // Velocidad de escritura (40ms por letra)
       } else {
-        p.classList.remove("estrofa-tipeando");
+        p.classList.remove("estrofa-tipeando"); // Quitamos el cursor al terminar la estrofa
         indiceEstrofa++;
-        setTimeout(escribirEstrofa, 500);
+        setTimeout(escribirEstrofa, 600); // Pausa de 600ms antes de empezar la siguiente estrofa
       }
     }
 
@@ -58,6 +41,7 @@ function escribirEstrofa() {
   }
 }
 
+// Inicia el efecto cuando la página carga
 window.onload = () => {
   setTimeout(escribirEstrofa, 500);
 };
